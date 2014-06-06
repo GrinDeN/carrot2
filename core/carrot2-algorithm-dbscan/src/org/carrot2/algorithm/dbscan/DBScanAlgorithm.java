c951677ef9c064da92f4fcd17cac5576a8a2958c
trocha6 <trocha6@gmail.com>
2014-06-06 12:21:28 +0200

    Poprawki dbscan

--- core/carrot2-algorithm-dbscan/src/org/carrot2/algorithm/dbscan/DBScanAlgorithm.java
+++ core/carrot2-algorithm-dbscan/src/org/carrot2/algorithm/dbscan/DBScanAlgorithm.java
@@ -3,8 +3,10 @@ package org.carrot2.algorithm.dbscan;
 import java.util.*;
 import java.math.*;
 
+import org.apache.mahout.math.function.Functions;
 import org.apache.mahout.math.matrix.DoubleMatrix1D;
 import org.apache.mahout.math.matrix.DoubleMatrix2D;
+import org.apache.mahout.math.matrix.impl.DenseDoubleMatrix1D;
 import org.carrot2.core.*;
 import org.carrot2.core.attribute.*;
 import org.carrot2.util.IntArrayPredicateIteratorTest;
@@ -27,6 +29,8 @@ import com.carrotsearch.hppc.IntArrayList;
 import com.carrotsearch.hppc.IntIntOpenHashMap;
 import com.carrotsearch.hppc.cursors.IntCursor;
 import com.carrotsearch.hppc.cursors.IntIntCursor;
+import com.carrotsearch.hppc.sorting.IndirectComparator;
+import com.carrotsearch.hppc.sorting.IndirectSort;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
@@ -70,11 +74,22 @@ public class DBScanAlgorithm extends ProcessingComponentBase implements
     @Input
     @Processing
     @Attribute
-    @DoubleRange(min = 0.0, max = 300.00)
+    @DoubleRange(min = 0.0, max = 500.00)
     @Group("Fields")
     @Level(AttributeLevel.BASIC)
     @Label("Eps")    
-    public double eps = 1.5;
+    public double eps = 150;
+    /**
+     * Label count. The minimum number of labels to return for each cluster.
+     */
+    @Processing
+    @Input
+    @Attribute
+    @IntRange(min = 1, max = 10)
+    @Group(DefaultGroups.CLUSTERS)
+    @Level(AttributeLevel.BASIC)
+    @Label("Label count")
+    public int labelCount = 3;
     
     public IPreprocessingPipeline preprocessingPipeline = new BasicPreprocessingPipeline();
 
@@ -153,7 +168,6 @@ public class DBScanAlgorithm extends ProcessingComponentBase implements
         final DoubleMatrix2D tdMatrix;
         tdMatrix = vsmContext.termDocumentMatrix;
         
-        IntArrayList group = new IntArrayList();
         TreeSet<Integer> unvisited = new TreeSet<Integer>();
         final List<IntArrayList> rawClusters = Lists.newArrayList();
         IntArrayList noise = new IntArrayList();
@@ -172,24 +186,70 @@ public class DBScanAlgorithm extends ProcessingComponentBase implements
         		rawClusters.add(newCluster);
         	}
         }
-        
         for (int i = 0; i < rawClusters.size(); i++)
         {
             final Cluster cluster = new Cluster();
-            String a = new String("temp"+i);
+
             final IntArrayList rawCluster = rawClusters.get(i);
-            cluster.addPhrases(a);
-            for (int j = 0; j < rawCluster.size(); j++)
+            if (rawCluster.size() > 1)
             {
-                cluster.addDocuments(documents.get(rawCluster.get(j)));
+                cluster.addPhrases(getLabels(rawCluster,
+                    vsmContext.termDocumentMatrix, rowToStemIndex,
+                    preprocessingContext.allStems.mostFrequentOriginalWordIndex,
+                    preprocessingContext.allWords.image));
+                for (int j = 0; j < rawCluster.size(); j++)
+                {
+                    cluster.addDocuments(documents.get(rawCluster.get(j)));
+                }
+                clusters.add(cluster);
             }
-            clusters.add(cluster);
         }
+
 
     Collections.sort(clusters, Cluster.BY_REVERSED_SIZE_AND_LABEL_COMPARATOR);
     Cluster.appendOtherTopics(documents, clusters);
         
     }
+    
+    private List<String> getLabels(IntArrayList documents,
+            DoubleMatrix2D termDocumentMatrix, IntIntOpenHashMap rowToStemIndex,
+            int [] mostFrequentOriginalWordIndex, char [][] wordImage)
+    {
+        final DoubleMatrix1D words = new DenseDoubleMatrix1D(termDocumentMatrix.rows());
+        for (IntCursor d : documents)
+        {
+            words.assign(termDocumentMatrix.viewColumn(d.value), Functions.PLUS);
+        }
+        final List<String> labels = Lists.newArrayListWithCapacity(labelCount);
+        final int [] order = IndirectSort.mergesort(0, words.size(),
+            new IndirectComparator()
+            {
+                @Override
+                public int compare(int a, int b)
+                {
+                    final double valueA = words.get(a);
+                    final double valueB = words.get(b);
+                    return valueA < valueB ? -1 : valueA > valueB ? 1 : 0;
+                }
+            });
+        final double minValueForLabel = words.get(order[order.length
+            - Math.min(labelCount, order.length)]);
+        for (int i = 0; i < words.size(); i++)
+        {
+            if (words.getQuick(i) >= minValueForLabel)
+            {
+                labels.add(LabelFormatter.format(new char [] []
+                {
+                    wordImage[mostFrequentOriginalWordIndex[rowToStemIndex.get(i)]]
+                }, new boolean []
+                {
+                    false
+                }, false));
+            }
+        }
+        return labels;
+    }
+    
     double getDistance(DoubleMatrix1D a, DoubleMatrix1D b) {
     	double dist=0;
     	for (int i=0; i< a.size(); ++i) {
@@ -200,10 +260,9 @@ public class DBScanAlgorithm extends ProcessingComponentBase implements
     
     IntArrayList regionQuery(int P, DoubleMatrix2D tdMatrix) {
     	IntArrayList neighbor = new IntArrayList();
-    	neighbor.add(P);
     	for(int i = 0; i<tdMatrix.columns(); ++i) {
     		if(i==P)
-    			continue;
+    			neighbor.add(P);
     		if(getDistance(tdMatrix.viewColumn(i), tdMatrix.viewColumn(P)) <= eps)
     			neighbor.add(i);
     	}
@@ -229,5 +288,4 @@ public class DBScanAlgorithm extends ProcessingComponentBase implements
     	}
     	return cluster;
     }
-    
 }
